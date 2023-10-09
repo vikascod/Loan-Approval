aa6eeae7891f936798ec2444c4f0f6ccb17ea9b8
Vikas <vikas1618072@gmail.com>
2023-10-09 19:58:23 +0530

    update reamd.md

--- README.md
+++ README.md
@@ -1,1 +1,65 @@
-## Loan Approval prediction
+# Loan Approval Prediction
+
+## Overview
+The Loan Approval Prediction project aims to predict whether a loan application will be approved or not based on various features.
+
+## Table of Contents
+1. [Project Setup](#1-project-setup)
+   - [Prerequisites](#prerequisites)
+   - [Installation](#installation)
+2. [Data](#2-data)
+   - [Data Source](#data-source)
+   - [Data Description](#data-description)
+3. [Data Preprocessing](#3-data-preprocessing)
+   - [Data Cleaning](#data-cleaning)
+   - [Feature Engineering](#feature-engineering)
+4. [Model Training](#4-model-training)
+   - [Classification Models](#classification-models)
+   - [Hyperparameter Tuning](#hyperparameter-tuning)
+5. [Model Evaluation](#5-model-evaluation)
+   - [Metrics](#metrics)
+   - [Best Model Selection](#best-model-selection)
+
+## 1. Project Setup
+
+### Prerequisites
+Before getting started, ensure you have the following:
+- Python (>=3.6) installed
+- Python virtual environment (optional but recommended)
+- Necessary packages and libraries installed (pip install -r requirements.txt)
+
+### Installation
+1. Clone this repository:
+    ```shell
+    git clone https://github.com/vikascod/Loan-Approven-EndtoEnd-Project.git
+    cd Loan-Approven-EndtoEnd-Project
+
+
+## 2. Data
+Data Source
+The dataset used in this project is sourced from [data_source_url]. You can download it from [data_source_download_link] and place it in the data/ directory.
+
+Data Description
+The dataset contains information about loan applicants, including features like gender, marital status, education, income, and more. The target variable is whether the loan was approved or not.
+
+## 3. Data Preprocessing
+Data Cleaning
+Handle missing values.
+Remove duplicates.
+Handle outliers if necessary.
+Feature Engineering
+Create new features if relevant.
+Encode categorical variables.
+Normalize or scale numerical variables.
+
+## 4. Model Training
+Classification Models
+Train various classification models such as Random Forest, Decision Tree, Logistic Regression, etc.
+Hyperparameter Tuning
+Use hyperparameter tuning techniques to optimize model performance.
+
+## 5. Model Evaluation
+Metrics
+Evaluate models using classification metrics like accuracy, precision, recall, F1-score, and ROC-AUC.
+Best Model Selection
+Select the best-performing model based on evaluation metrics.
---
+++ src/components/model_trainer.py
@@ -0,0 +1,112 @@
+import os
+import sys
+from dataclasses import dataclass
+
+from sklearn.ensemble import (
+    AdaBoostClassifier,
+    GradientBoostingClassifier,
+    RandomForestClassifier,
+)
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score, classification_report
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.tree import DecisionTreeClassifier
+from xgboost import XGBClassifier
+from catboost import CatBoostClassifier
+
+from src.exception import CustomException
+from src.logger import logging
+
+from src.utils import save_object
+from src.evaluate import evaluate_models
+
+@dataclass
+class ModelTrainerConfig:
+    """
+    Configuration class for the ModelTrainer, specifying the path for saving the trained model.
+    """
+    trained_model_file_path = os.path.join("artifacts", "model.pkl")
+
+class ModelTrainer:
+    """
+    Class responsible for training and evaluating classification models.
+    """
+    def __init__(self):
+        self.model_trainer_config = ModelTrainerConfig()
+
+    def initiate_model_trainer(self, train_array, test_array):
+        try:
+            logging.info("Split training and test input data")
+            X_train, y_train, X_test, y_test = (
+                train_array[:, :-1],
+                train_array[:, -1],
+                test_array[:, :-1],
+                test_array[:, -1]
+            )
+            models = {
+                "Random Forest": RandomForestClassifier(),
+                "Decision Tree": DecisionTreeClassifier(),
+                "Gradient Boosting": GradientBoostingClassifier(),
+                "Logistic Regression": LogisticRegression(),
+                "XGBClassifier": XGBClassifier(),
+                "CatBoostClassifier": CatBoostClassifier(verbose=False),
+                "AdaBoost Classifier": AdaBoostClassifier(),
+            }
+            params = {
+                "Decision Tree": {
+                    'criterion': ['gini', 'entropy'],
+                    'splitter': ['best', 'random'],
+                    'max_depth': [None, 5, 10, 15],
+                },
+                "Random Forest": {
+                    'n_estimators': [8, 16, 32, 64, 128, 256],
+                    'criterion': ['gini', 'entropy'],
+                    'max_depth': [None, 5, 10, 15],
+                },
+                "Gradient Boosting": {
+                    'learning_rate': [0.1, 0.01, 0.05, 0.001],
+                    'n_estimators': [8, 16, 32, 64, 128, 256],
+                    'max_depth': [3, 5, 7, 9],
+                },
+                "Logistic Regression": {},
+                "XGBClassifier": {
+                    'learning_rate': [0.1, 0.01, 0.05, 0.001],
+                    'n_estimators': [8, 16, 32, 64, 128, 256],
+                    'max_depth': [3, 5, 7, 9],
+                },
+                "CatBoostClassifier": {
+                    'depth': [6, 8, 10],
+                    'learning_rate': [0.01, 0.05, 0.1],
+                    'iterations': [30, 50, 100],
+                },
+                "AdaBoost Classifier": {
+                    'learning_rate': [0.1, 0.01, 0.5, 0.001],
+                    'n_estimators': [8, 16, 32, 64, 128, 256],
+                }
+            }
+
+            model_report = evaluate_models(X_train=X_train, y_train=y_train, X_test=X_test, y_test=y_test,
+                                           models=models, param=params)
+
+            best_model_name = max(model_report, key=model_report.get)
+            best_model = models[best_model_name]
+
+            if model_report[best_model_name]['accuracy'] < 0.6:
+                raise CustomException("No best model found")
+
+            logging.info(f"Best found model on both training and testing dataset: {best_model_name}")
+
+            save_object(
+                file_path=self.model_trainer_config.trained_model_file_path,
+                obj=best_model
+            )
+
+            predicted = best_model.predict(X_test)
+
+            accuracy = accuracy_score(y_test, predicted)
+            classification_report_str = classification_report(y_test, predicted)
+
+            return accuracy, classification_report_str
+
+        except Exception as e:
+            raise CustomException(e, sys)
